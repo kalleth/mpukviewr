90412a297151173399122a5346b2197f1a8d3b3e
Tom Russell <tom@cishub.co.uk>
2012-07-08 18:42:34 +0000

    More translation to CS. Notifier is now fleshed out for sound, popup and title notifications.

--- viewr/app/assets/javascripts/exp/notifier.coffee
+++ viewr/app/assets/javascripts/exp/notifier.coffee
@@ -1,15 +1,42 @@
 class window.viewr.Notifier
   
   constructor: (@settings) ->
+    @resetUnseen()
 
   notify: (payload) ->
     # Calls the other 3 methods based on settings
     console.log "Notifying: ", payload
+    @soundNotify() if @settings.sounds_enabled
+    @popupNotify(payload) if @settings.notifications_enabled
+    @titleNotify()
 
   notifyError: (error) ->
   
   soundNotify: ->
+    $("audio")[0].play()
 
   popupNotify: (payload) ->
+    if window.webkitNotifications
+      if window.webkitNotifications.checkPermission() < 1
+        console.log "Blah"
+        title = @extractText("#{payload.etype}: #{payload.title}")
+        icon = "http://viewr.multiplay.co.uk/images/icons/#{payload.etype.toLowerCase()}.png"
+        body = @extractText(payload.description)
+        popup = window.webkitNotifications.createNotification(icon, title, body)
+        popup.show()
+        setTimeout(->
+          popup.cancel()
+        , '15000')
 
   titleNotify: ->
+    if !window.window_focus
+      @unseen++
+      document.title = "(#{@unseen}) Viewr"
+
+  resetUnseen: ->
+    @unseen = 0
+
+  extractText: (fragment) ->
+    div = document.createElement('div')
+    div.innerHTML = fragment
+    div.textContent || div.innerText || fragment
--- viewr/app/assets/javascripts/exp/vpop.js
+++ viewr/app/assets/javascripts/exp/vpop.js
@@ -1,45 +1,8 @@
-var window_focus = true;
+window.window_focus = true;
 var since_defocused = 0;
 
-function alertUser(evt) {
-  var klass = evt.etype + '_enabled';
-  if(window.Settings[klass.toLowerCase()] === true) {
-    if(!window_focus) {
-      since_defocused++;
-      document.title = "("+since_defocused+") Viewr";
-    }
-    if(window.Settings.sounds_enabled === true) {
-      $("audio")[0].play();
-    }
-    if(window.webkitNotifications && window.Settings.notifications_enabled === true) {
-      dtopNotify(evt);
-    }
-  }
-}
-
-function textFromHTML(data) {
-  var div = document.createElement('div');
-  div.innerHTML = data;
-  var body = div.textContent || div.innerText || data;
-  return body;
-}
-
-function dtopNotify(evt) {
-  if (window.webkitNotifications.checkPermission() < 1) {
-    var title = evt.etype + ": " + evt.title;
-    title = textFromHTML(title);
-    var icon = "http://viewr.multiplay.co.uk/images/icons/" + evt.etype.toLowerCase() + ".png";
-    var body = textFromHTML(evt.description);
-    var popup = window.webkitNotifications.createNotification(icon, title, body);
-    popup.show();
-    setTimeout(function() {
-      popup.cancel();
-    }, '15000');
-  }
-}
-
 function fadeUnseen() {
-  if(window_focus) {
+  if(window.window_focus) {
     setTimeout(function() {
       $("li.unseen").each(function() {
         $(this).animate({backgroundColor:"#f8f8f8"}, 2000, function() {
@@ -84,8 +47,7 @@ function checkListener (evt) {
 }
 
 function onWindowFocus() {
-  window_focus = true;
-  since_defocused = 0;
+  window.window_focus = true;
   document.title = "Viewr";
   fadeUnseen();
 }
@@ -93,41 +55,9 @@ function onWindowFocus() {
 dbOnWindowFocus = onWindowFocus.debounce(250, true);
 
 function onWindowLoseFocus() {
-  window_focus = false;
+  window.window_focus = false;
 }
 dbOnWindowLoseFocus = onWindowLoseFocus.debounce(250, true);
-
-/*function showEvent(evt) {
-  var icon = '<div class="icon" title="'+evt.etype+'"></div>';
-  if(evt.etype == "NEWS") {
-    var title = '<p class="news_title"><a href="'+evt.uid+'" target="_blank">'+evt.title+'</a></p>'
-  } else {
-    var title = '<p class="news_title">' + evt.title + '</p>';
-  }
-  var thedate = new Date(evt.happened_at);
-  var tmfield = '<time class="timeago" data-title="' + thedate.strftime("%d/%m/%y %H:%M") + '" datetime="' + evt.happened_at + '">Now</time>';
-  var time = '<p class="time">'+tmfield+'</p>';
-  var body = '<p class="news_body">' + evt.description + '</p>';
-  var li = '<li id="added_event" class="news_item hidden unseen ' + evt.etype.toLowerCase() + '">' + icon + title + time + body + '</li>';
-  console.log("Inserting:", li)
-  $('#news_container ul').prepend($(li));
-  var klass = evt.etype + '_enabled';
-  if(window.Settings[klass.toLowerCase()] === true) {
-    $('#added_event').fadeIn(800, function() {
-      $('#added_event').attr('style', '');
-    });
-  }
-  $('#added_event time').tooltip({
-    placement: 'left'
-  });
-  $('#added_event time').timeago();
-  $('#added_event').attr('id','');
-  //remove the last child as well
-  $('#news_container ul li:last-child').fadeOut(800, function() {
-    $('#news_container ul li:last-child').remove();
-  });
-  alertUser(evt);
-}*/
 
 $(document).ready(function() {
   $("#notifications_enabled").bind('click', function() {
@@ -137,11 +67,6 @@ $(document).ready(function() {
   });
   $("time.timeago").timeago();
   $("time").tooltip({placement: 'left'});
-/*  var faye_url = "http://" + window.location.hostname + ":9292/faye";
-  var client = new Faye.Client(faye_url);
-  var s = client.subscribe('/messages', function(evt) {
-    showEvent(evt);
-  });*/
   $("#toggle_settings").bind('click', function() {
     $("#settings_container").fadeToggle('slow', 'linear');
   });
